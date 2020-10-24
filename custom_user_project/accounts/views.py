d26356fae73c0bf0a298be24ff16744548bbd7bc
Lodrick Thomas <lodrick_official@hotmail.com>
2020-10-24 17:10:21 +0300

    second push

---
+++ custom_user_project/accounts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from django.shortcuts import render, redirect
+from .forms import UserAdminCreationForm
+
+
+def registerView(req):
+    form = UserAdminCreationForm()
+    if req.method == 'POST':
+        form = UserAdminCreationForm(req.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('register')
+    return render(req, 'register.html', {'form': form})
